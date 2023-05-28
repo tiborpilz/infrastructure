f157794307cb9add4e9142cd3d82a0aefdb425da
Tibor Pilz <tibor@pilz.berlin>
2023-05-28 13:44:36 +0200

    Reorder & Renaming

---
+++ terragrunt/extensions/main.tf
@@ -0,0 +1,59 @@
+variable "secrets" {
+  type    = map(string)
+  default = {}
+}
+
+variable "cluster_connection" {
+  type    = map(string)
+  default = {}
+}
+
+variable "kube_config_yaml" {}
+
+variable "gitlab_token" {}
+# variable "gitlab_project_id" {}
+variable "domain" {}
+variable "email" {}
+terraform {
+  required_providers {
+    gitlab = {
+      source = "gitlabhq/gitlab"
+    }
+    kustomization = {
+      source  = "kbst/kustomization"
+      version = "0.9.0"
+    }
+    keycloak = {
+      source  = "mrparkers/keycloak"
+      version = "4.1.0"
+    }
+    rancher2 = {
+      source  = "rancher/rancher2"
+      version = "3.0.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = var.cluster_connection.host
+  client_certificate     = var.cluster_connection.client_certificate
+  client_key             = var.cluster_connection.client_key
+  cluster_ca_certificate = var.cluster_connection.cluster_ca_certificate
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = var.cluster_connection.host
+    client_certificate     = var.cluster_connection.client_certificate
+    client_key             = var.cluster_connection.client_key
+    cluster_ca_certificate = var.cluster_connection.cluster_ca_certificate
+  }
+}
+
+provider "kustomization" {
+  kubeconfig_raw = var.kube_config_yaml
+}
+
+provider "gitlab" {
+  token = var.gitlab_token
+}
